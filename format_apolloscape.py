84459f6a2e3b21a2e8a28e8fba1e41ed2236fd9a
Han-sss <sunhan240015406@gmail.com>
2021-10-04 16:01:24 +0900

    update the data generation part, saved at data/Apolloscape/prediction_***/formatted

--- format_apolloscape.py
+++ format_apolloscape.py
@@ -126,8 +126,8 @@ def generate_data(file_name, data_type):
             
             per_frame_node_matrix.append(one_frame_node_matrix)
 
-        print("makr!")
-        packaged_sequences = []
+        packaged_input_sequences = []
+        packaged_result_sequences = []
         for object_traj_sequence in cleaned_objects_sequence:
             object_idx = int(object_traj_sequence[0][1])
             frame_idx = int(object_traj_sequence[0][0])
@@ -135,13 +135,21 @@ def generate_data(file_name, data_type):
             node_matrix_sequence = torch.stack(per_frame_node_matrix[frame_idx: frame_idx+total_len] ,dim=0)
             graph_sequence = per_frame_graphs[frame_idx: frame_idx+total_len]
 
-            one_packaged_sequence = {'trajectory': object_traj_sequence, 
-                                     'node_matrix': node_matrix_sequence, 
-                                     'graph': graph_sequence}
+            one_packaged_sequence = {}
+            one_packaged_sequence[0] = {'trajectory': object_traj_sequence[0:observe_len], 
+                                        'node_matrix': node_matrix_sequence[0:observe_len], 
+                                        'graph': graph_sequence[0:observe_len]}
+
+            one_packaged_sequence[1] = {'trajectory': object_traj_sequence[observe_len:total_len], 
+                                        'node_matrix': node_matrix_sequence[observe_len:total_len], 
+                                        'graph': graph_sequence[observe_len:total_len]}
             
-            packaged_sequences.append(one_packaged_sequence)
-        
-        return packaged_sequences
+            packaged_input_sequences.append(one_packaged_sequence[0])
+            packaged_result_sequences.append(one_packaged_sequence[1])
+
+        file_name = file_name.split('/')
+        print("train set "+file_name[-1]+" generation finished!")        
+        return packaged_input_sequences, packaged_result_sequences
 
     elif data_type == 'test':
         
@@ -149,13 +157,13 @@ def generate_data(file_name, data_type):
         
         return torch.zeros(1,) 
 
-def apolloscape_to_formatted(DATA_DIR, DATA_DIR_TEST, data_type):
+def apolloscape_to_formatted(DATA_DIR_TRAIN, DATA_DIR_TEST, data_type):
 
     
     train_file_names = []
     test_file_names = []
 
-    for file in sorted(os.listdir(DATA_DIR)):
+    for file in sorted(os.listdir(DATA_DIR_TRAIN)):
         if file.endswith('.txt'):
             train_file_names.append(file)
 
@@ -166,14 +174,24 @@ def apolloscape_to_formatted(DATA_DIR, DATA_DIR_TEST, data_type):
 
     if data_type == 'train':
 
+        generated_input = []
+        generated_result = []
         for train_file_name in train_file_names:
-            file_name = DATA_DIR + train_file_name 
-            generated_data = generate_data(file_name, data_type)
+            file_name = DATA_DIR_TRAIN + train_file_name 
+            generated_one_input, generated_one_result = generate_data(file_name, data_type)
+            generated_input += generated_one_input
+            generated_result += generated_one_result
 
+        to_save_input_txt = DATA_DIR_TRAIN + 'formatted/train_input.npy'
+        to_save_result_txt = DATA_DIR_TRAIN + 'formatted/train_result.npy'        
 
-        to_save_txt = './resources/data/' + 'APOL/train/trainSet0.npy'
+        print(len(generated_input))
+        # for input in generated_input:
+        #     print(input['trajectory'].size())
+        #     print(input['node_matrix'].size())
 
-        save_to_file( generated_data, to_save_txt)
+        save_to_file( generated_input, to_save_input_txt)
+        save_to_file( generated_result, to_save_result_txt)
 
     #     save_to_text(formatted_data, to_save_txt)
 
@@ -185,7 +203,7 @@ def apolloscape_to_formatted(DATA_DIR, DATA_DIR_TEST, data_type):
         generated_data = generate_data(file, data_type)
 
 
-        to_save_txt = './resources/data/' + 'APOL/val/valSet0.npy'
+        to_save_txt = './data/APOL/val/valSet0.npy'
 
         save_to_file( generated_data, to_save_txt)
 
@@ -213,9 +231,9 @@ DATA_DIR = folder_where_unzipped_apolloscape_data_is_present + '/sample_trajecto
 DATA_DIR_TEST = folder_where_unzipped_apolloscape_data_is_present + '/prediction_test/'
 '''
 dir = '/home/mount/GCN-lstm/data/Aplloscape'
-DATA_DIR = dir + '/prediction_train/'
+DATA_DIR_TRAIN = dir + '/prediction_train/'
 DATA_DIR_TEST = dir + '/prediction_test/'
 
 data_type = 'train' #train, test
 
-apolloscape_to_formatted(DATA_DIR, DATA_DIR_TEST, data_type)
+apolloscape_to_formatted(DATA_DIR_TRAIN, DATA_DIR_TEST, data_type)
